852fa840f2e9db3807f40a61368a3cb4aa29b4ed
LAPTOP-VNSHFLT4\Viv <e0031039@u.nus.edu>
2017-09-26 16:45:45 +0800

    test 2.

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -39,7 +39,7 @@ import seedu.address.ui.UiManager;
  * The main entry point to the application.
  */
 
-//test
+//test 
 public class MainApp extends Application {
 
     public static final Version VERSION = new Version(0, 6, 0, true);
