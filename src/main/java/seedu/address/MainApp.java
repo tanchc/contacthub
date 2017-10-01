d5c13b27a3d201353003ae11be87e56dc9995620
LAPTOP-VNSHFLT4\Viv <e0031039@u.nus.edu>
2017-10-01 15:54:39 +0800

    removed a line

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -38,7 +38,6 @@ import seedu.address.ui.UiManager;
 /**
  * The main entry point to the application.
  */
-
 public class MainApp extends Application {
 
     public static final Version VERSION = new Version(0, 6, 0, true);
