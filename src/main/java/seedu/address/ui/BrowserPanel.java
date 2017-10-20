2553d58f13f6d9d12977838527a799c48ae2fb19
tanchc <tanchc16@gmail.com>
2017-10-21 00:58:52 +0800

    Cleaned up code

--- src/main/java/seedu/address/ui/BrowserPanel.java
+++ src/main/java/seedu/address/ui/BrowserPanel.java
@@ -58,25 +58,27 @@ public class BrowserPanel extends UiPart<Region> {
      */
     private void loadAddressPage(ReadOnlyPerson person) throws IOException {
         ClassLoader classLoader = getClass().getClassLoader();
-        File htmlTemplateFile = new File(classLoader.getResource("view/LocatedAddress.html").getFile());
-        resetPage(htmlTemplateFile);
-        String htmlString = FileUtils.readFileToString(htmlTemplateFile);
+        File locatedAddressFile = new File(classLoader.getResource("view/LocatedAddress.html").getFile());
+        File htmlTemplateFile = new File(classLoader.getResource("view/Template.html").getFile());
+        resetPage(htmlTemplateFile, locatedAddressFile);
+        String htmlString = FileUtils.readFileToString(locatedAddressFile);
         System.out.println(htmlString);
         String title = "New Page";
         int stopIndex = person.getAddress().getGMapsAddress().indexOf(',');
         String address = person.getAddress().getGMapsAddress().substring(0, stopIndex);
         System.out.println(address);
-        String body = "<div class=" + "\"" + "mapouter" + "\"" + "><div class=" + "\"" + "gmap_canvas" + "\""
-                + "><iframe width=" + "\"" + "600" + "\"" + " height=" + "\"" + "500" + "\"" + " id=" + "\""
-                + "gmap_canvas" + "\"" + " src= " +  "\"" + "https://maps.google.com/maps?q=" + address
-                + "&t=&z=13&ie=UTF8&iwloc=&output=embed" + "\"" + " frameborder=" + "\"" + "0" + "\"" + " scrolling="
-                + "\"" + "no" + "\"" + " marginheight=" + "\"" + "0" + "\"" + " marginwidth=" + "\"" + "0" + "\""
-                + "></iframe>google maps einbinden <a href=" + "\"" + "http://www.pureblack.de/google-maps/" + "\""
-                + ">pureblack.de</a></div><style>.mapouter{overflow:hidden;height:500px;width:600px;}.gmap_canvas "
-                + "{background:none!important;height:500px;width:600px;}</style></div>";
-        htmlString = htmlString.replace("$body", body);
+//        String body = "<div class=" + "\"" + "mapouter" + "\"" + "><div class=" + "\"" + "gmap_canvas" + "\""
+//                + "><iframe width=" + "\"" + "600" + "\"" + " height=" + "\"" + "500" + "\"" + " id=" + "\""
+//                + "gmap_canvas" + "\"" + " src= " +  "\"" + "https://maps.google.com/maps?q=" + address
+//                + "&t=&z=13&ie=UTF8&iwloc=&output=embed" + "\"" + " frameborder=" + "\"" + "0" + "\"" + " scrolling="
+//                + "\"" + "no" + "\"" + " marginheight=" + "\"" + "0" + "\"" + " marginwidth=" + "\"" + "0" + "\""
+//                + "></iframe>google maps einbinden <a href=" + "\"" + "http://www.pureblack.de/google-maps/" + "\""
+//                + ">pureblack.de</a></div><style>.mapouter{overflow:hidden;height:500px;width:600px;}.gmap_canvas "
+//                + "{background:none!important;height:500px;width:600px;}</style></div>";
         System.out.println(htmlString);
-        FileUtils.writeStringToFile(htmlTemplateFile, htmlString);
+        htmlString = htmlString.replace("$body", address.replace(" ", "+"));
+        System.out.println(htmlString);
+        FileUtils.writeStringToFile(locatedAddressFile, htmlString);
         URL addressPage = MainApp.class.getResource(FXML_FILE_FOLDER + ADDRESS_PAGE);
         loadPage(addressPage.toExternalForm());
     }
@@ -84,19 +86,9 @@ public class BrowserPanel extends UiPart<Region> {
     /**
      * resets the address page.
      */
-    private void resetPage(File file) throws IOException {
-        String reset = "<!DOCTYPE html>\n"
-                + "<html lang=\"en\">\n"
-                + "<head>\n"
-                + "    <link rel=\"stylesheet\" href=\"DarkTheme.css\">"
-                + "    <meta charset=\"UTF-8\">\n"
-                + "    <title>Title</title>\n"
-                + "</head>\n"
-                + "<body class = background>\n"
-                + "$body\n"
-                + "</body>\n"
-                + "</html>";
-        FileUtils.writeStringToFile(file, reset);
+    private void resetPage(File resetFile, File addressFile) throws IOException {
+        String reset = FileUtils.readFileToString(resetFile);
+        FileUtils.writeStringToFile(addressFile, reset);
     }
 
     public void loadPage(String url) {
