b7c0b21f5c34f7994add0d5ee1d9c16c9f4536a9
tanchc <tanchc16@gmail.com>
2017-10-21 00:24:00 +0800

    Added whitespaces and improved coding style

--- src/main/java/seedu/address/ui/BrowserPanel.java
+++ src/main/java/seedu/address/ui/BrowserPanel.java
@@ -66,14 +66,14 @@ public class BrowserPanel extends UiPart<Region> {
         int stopIndex = person.getAddress().getGMapsAddress().indexOf(',');
         String address = person.getAddress().getGMapsAddress().substring(0, stopIndex);
         System.out.println(address);
-        String body = "<div class=" +"\"" + "mapouter" + "\"" + "><div class=" + "\"" + "gmap_canvas" + "\""
+        String body = "<div class=" + "\"" + "mapouter" + "\"" + "><div class=" + "\"" + "gmap_canvas" + "\""
                 + "><iframe width=" + "\"" + "600" + "\"" + " height=" + "\"" + "500" + "\"" + " id=" + "\""
                 + "gmap_canvas" + "\"" + " src= " +  "\"" + "https://maps.google.com/maps?q=" + address
-                +"&t=&z=13&ie=UTF8&iwloc=&output=embed" + "\"" + " frameborder=" + "\"" + "0" + "\"" + " scrolling="
+                + "&t=&z=13&ie=UTF8&iwloc=&output=embed" + "\"" + " frameborder=" + "\"" + "0" + "\"" + " scrolling="
                 + "\"" + "no" + "\"" + " marginheight=" + "\"" + "0" + "\"" + " marginwidth=" + "\"" + "0" + "\""
                 + "></iframe>google maps einbinden <a href=" + "\"" + "http://www.pureblack.de/google-maps/" + "\""
-                + ">pureblack.de</a></div><style>.mapouter{overflow:hidden;height:500px;width:600px;}.gmap_canvas " +
-                "{background:none!important;height:500px;width:600px;}</style></div>";
+                + ">pureblack.de</a></div><style>.mapouter{overflow:hidden;height:500px;width:600px;}.gmap_canvas "
+                + "{background:none!important;height:500px;width:600px;}</style></div>";
         htmlString = htmlString.replace("$body", body);
         System.out.println(htmlString);
         FileUtils.writeStringToFile(htmlTemplateFile, htmlString);
