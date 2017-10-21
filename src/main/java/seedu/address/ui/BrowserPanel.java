f65a1ec1090c54ac63da0b9c4d781d80621f3840
tanchc <tanchc16@gmail.com>
2017-10-22 01:42:39 +0800

    Uncommented out some lines due to testing error

--- src/main/java/seedu/address/ui/BrowserPanel.java
+++ src/main/java/seedu/address/ui/BrowserPanel.java
@@ -26,10 +26,10 @@ import seedu.address.model.person.ReadOnlyPerson;
  */
 public class BrowserPanel extends UiPart<Region> {
 
-    private static final String DEFAULT_PAGE = "default.html";
+    public static final String DEFAULT_PAGE = "default.html";
     private static final String ADDRESS_PAGE = "LocatedAddress.html";
-    //public static final String GOOGLE_SEARCH_URL_PREFIX = "https://www.google.com.sg/search?safe=off&q=";
-    //public static final String GOOGLE_SEARCH_URL_SUFFIX = "&cad=h";
+    public static final String GOOGLE_SEARCH_URL_PREFIX = "https://www.google.com.sg/search?safe=off&q=";
+    public static final String GOOGLE_SEARCH_URL_SUFFIX = "&cad=h";
 
     private static final String FXML = "BrowserPanel.fxml";
 
